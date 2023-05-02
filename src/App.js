b8a731bf719e0847d04b611e640465c7f997f90f
İpek Ülfet Kaylan <ipekulfetkaylan@gmail.com>
2023-05-02 13:51:08 +0300

    react router added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 // import Topbar from "./components/topbar/Topbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import Profile from "./components/profile/Profile";
 
 // import Register from "./components/register/Register";
 
@@ -8,9 +12,18 @@ import Home from "./pages/home/Home";
 // import Profile from "./components/profile/Profile";
 
 
+
 function App() {
   return (
-   <Home/>
+    <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/register" element={<Register/>}></Route>
+      <Route path="/profile" element={<Profile/>}></Route>
+     </Routes>
+    </BrowserRouter>
+   
   );
 }
 
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,43 +1,58 @@
-import "./topbar.css"
-import SearchIcon from '@mui/icons-material/Search';
-import PersonIcon from '@mui/icons-material/Person';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import ChatIcon from '@mui/icons-material/Chat';
+import "./topbar.css";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import ChatIcon from "@mui/icons-material/Chat";
+import { Link } from "react-router-dom";
 export default function Topbar() {
   return (
     <div className="topbarContainer">
-        <div className="topbarLeft">
-            <span className="logo">SocialFriend</span>
+      <div className="topbarLeft">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">SocialFriend</span>
+        </Link>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchBar">
+          <SearchIcon className="searchIcon" />
+          <input
+            type="text"
+            placeholder="Search friends, post and video.."
+            className="searchInput"
+          />
         </div>
-        <div className="topbarCenter">
-            <div className="searchBar">
-                <SearchIcon className="searchIcon"/>
-                <input type="text" placeholder="Search friends, post and video.." className="searchInput" />
-
-            </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <Link to="/profile" style={{ textDecoration: "none", color:"#fff" }}>
+            <span className="topbarLink">Homepage</span>
+          </Link>
+          <Link to="/" style={{ textDecoration: "none", color:"#fff" }}>
+            <span className="topbarLink">TimeLine</span>
+          </Link>
         </div>
-        <div className="topbarRight">
-            <div className="topbarLinks">
-                <span className="topbarLink">HomePage</span>
-                <span className="topbarLink">TimeLine</span>
-            </div>
-            <div className="topbarIcon">
-                <div className="topbarItemIcon">
-                    <PersonIcon/>
-                    <span className="topbarIconBadge">1</span>
-                </div>
-                <div className="topbarItemIcon">
-                    <NotificationsIcon/>
-                    <span className="topbarIconBadge">3</span>
-                </div>
-                <div className="topbarItemIcon">
-                    <ChatIcon/>
-                    <span className="topbarIconBadge">1</span>
-                </div>
-            </div>
-            <img src="/assets/img/pexels-daniel-xavier-1239291.jpg" alt="person" className="topbarImg" />
+        <div className="topbarIcon">
+          <div className="topbarItemIcon">
+            <PersonIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarItemIcon">
+            <NotificationsIcon />
+            <span className="topbarIconBadge">3</span>
+          </div>
+          <div className="topbarItemIcon">
+            <ChatIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
         </div>
-    
+        <Link to="/profile">
+          <img
+            src="/assets/img/pexels-daniel-xavier-1239291.jpg"
+            alt="person"
+            className="topbarImg"
+          />
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
